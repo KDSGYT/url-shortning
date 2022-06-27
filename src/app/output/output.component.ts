3ceadab92cb7c642d68c654e715032a578aa1c38
Karan Pal Singh <kdsgyt@gmail.com>
2022-06-27 19:20:01 -0400

    Basic functionality of the web app is finished just need to add some cometic touch.

--- src/app/output/output.component.ts
+++ src/app/output/output.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ConvertUrlService } from '../convert-url.service';
 
 @Component({
   selector: 'app-output',
   templateUrl: './output.component.html',
   styleUrls: ['./output.component.scss']
 })
-export class OutputComponent implements OnInit {
+export class OutputComponent implements OnInit,OnDestroy {
 
-  constructor() { }
-
+  constructor(private URLService: ConvertUrlService) { }
+  public output: string = this.URLService.newUrl.getValue();
   ngOnInit(): void {
+    this.URLService.newUrl.subscribe(value => this.output = value);
+  }
+
+  public copyLinkToClipboard() {
+    navigator.clipboard.writeText(this.output)
+    alert('Copied to clipboard!!!')
+  }
+
+  ngOnDestroy(): void {
+    this.URLService.newUrl.unsubscribe();
   }
 
 }
