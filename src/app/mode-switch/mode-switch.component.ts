25f7e54f4c13e2a4dfa9f1658554c27528a68d91
Karan Pal Singh <kdsgyt@gmail.com>
2022-06-29 00:41:18 -0400

    Basic design of the website is done

--- src/app/mode-switch/mode-switch.component.ts
+++ src/app/mode-switch/mode-switch.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ThemeService } from 'src/theme.service';
 @Component({
   selector: 'app-mode-switch',
   templateUrl: './mode-switch.component.html',
   styleUrls: ['./mode-switch.component.scss']
 })
-export class ModeSwitchComponent implements OnInit {
+export class ModeSwitchComponent implements OnInit, OnDestroy{
 
   constructor(private ThemeService:ThemeService) { }
 
   changeMode = this.ThemeService.switchMode;
-  
+  public darkMode:boolean = this.ThemeService.darkmode.value;
   ngOnInit(): void {
+    this.ThemeService.darkmode.subscribe(value => this.darkMode = value)
+  }
+  ngOnDestroy():void {
+    this.ThemeService.darkmode.unsubscribe()
   }
 
 }
